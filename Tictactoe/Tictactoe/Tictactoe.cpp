43dfd00188344c739c9a8da2c3a123399432de0f
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-06-06 21:41:56 +0700

    Add check winning function

--- Tictactoe/Tictactoe/Tictactoe.cpp
+++ Tictactoe/Tictactoe/Tictactoe.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int gamemode, n, turn = rand() % 2, result = 3, slot = 0;
+int gamemode, n, turn = rand() % 2, result = 3, nmove = 0;
 char **a;
 char p1[20], p2[20], record[2];
 void CreateBoard()
@@ -22,13 +22,14 @@ void Marking()
 	if (turn)
 	{
 		a[i - 1][j - 1] = 'X';
-		turn = false;
+		turn = 0;
 	}
 	else
 	{
 		a[i - 1][j - 1] = 'O';
-		turn = true;
+		turn = 1;
 	}
+	nmove++;
 }
 void ShowCurrentBoard()
 {
@@ -53,13 +54,69 @@ void ShowCurrentBoard()
 	}
 	cout << str << "\n";;
 }
-void Check()
+bool Check()
 {
-	
+	bool k = true;
+	//check row
+	for (register int i = 0; i < n; i++)
+	{
+		for (register int j = 1; j < n; j++)
+		{
+			if (a[i][0] == ' ' || a[i][0] != a[i][j])
+			{
+				k = false;
+				break;
+			}
+			k = true;
+		}
+		if (k) return true;
+	}
+	//check column
+	for (register int i = 0; i < n; i++)
+	{
+		for (register int j = 1; j < n; j++)
+		{
+			if (a[0][i] == ' ' || a[0][i] != a[j][i])
+			{
+				k = false;
+				break;
+			}
+			k = true;
+		}
+		if (k) return true;
+	}
+	//check diagonal
+	for (register int i = 1; i < n; i++)
+	{
+		if (a[0][0] == ' ' || a[0][0] != a[i][i])
+		{
+			k = false;
+			break;
+		}
+		k = true;
+	}
+	if (k) return true;
+	for (register int i = 1; i < n; i++)
+	{
+		if (a[0][n - 1] == ' ' || a[0][n - 1] != a[i][n - 1 - i]) return false;
+	}
+	return true;
+}
+bool Result()
+{
+	if (Check())
+	{
+		if (!turn)
+			result = 0;
+		else result = 1;
+		return true;
+	}
+	else if (nmove == n*n) result = 2;
+	return false;
 }
 void Playing()
 {
-	while (result == 3)
+	while (nmove < n*n)
 	{
 		system("cls");
 		ShowCurrentBoard();
@@ -69,7 +126,14 @@ void Playing()
 		cin.ignore();
 		cin >> record;
 		Marking();
+		if (nmove >= 2 * n - 1 && Result()) break;
 	}
+}
+void DeleteArray()
+{
+	for (register int i = 0; i < n; i++)
+		delete[] a[i];
+	delete[] a;
 }
 void Game()
 {
@@ -90,10 +154,12 @@ void Game()
 		ShowCurrentBoard();
 		cout << "******************************************************************\n";
 		if (result == 2) cout << "DRAW\n";
-		else if (result == 0) cout << "PLAYER 1 WIN\n";
-		else if (result == 1) cout << "PLAYER 2 WIN\n";
+		else if (result == 0) cout << "PLAYER 1, " << p1 << " WIN\n";
+		else if (result == 1) cout << "PLAYER 2, " << p2 << " WIN\n";
 		turn = rand() % 2;
 		result = 3;
+		nmove = 0;
+		DeleteArray();
 		break;
 	default:
 		break;
