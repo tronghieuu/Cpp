0de4b3f272906752590da3a70c7612b75ebc324c
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-06-13 13:26:54 +0700

    + Declare elements and methods for DengueVirus class

--- Practice3_Oop/TheWarInHumanBody/TheWarInHumanBody/DengueVirus.h
+++ Practice3_Oop/TheWarInHumanBody/TheWarInHumanBody/DengueVirus.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef DENGUEVIRUS_H
+#define DENGUEVIRUS_H
+#include "Virus.h"
+class DengueVirus : public Virus
+{
+private:
+	char m_protein[4];
+public:
+	DengueVirus();
+	~DengueVirus();
+	DengueVirus(const DengueVirus* dv);
+	void DoBorn();
+	list<Virus*> DoClone();
+	void DoDie();
+	void InitResistance();
+};
+#endif
