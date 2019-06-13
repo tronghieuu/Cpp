001b0e205eb371f1bde128b326a6027169d824ea
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-06-13 14:19:25 +0700

    + Declare elements and methods for Patient class

--- Practice3_Oop/TheWarInHumanBody/TheWarInHumanBody/Patient.h
+++ Practice3_Oop/TheWarInHumanBody/TheWarInHumanBody/Patient.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef PATIENT_H
+#define PATIENT_H
+#include <list>
+#include "Virus.h"
+
+using namespace std;
+
+class Patient
+{
+private:
+	int m_resistance;
+	list<Virus*> m_virusList;
+	int m_state;
+public:
+	Patient();
+	~Patient();
+	void InitResistance();
+	void DoStart();
+	void TakeMedicine();
+	void DoDie();
+	int GetState();
+};
+#endif
